0fb26ee38b4c656f2898fb37620ab56db599d1b1
simon-towler <simon.towler@icloud.com>
2020-07-21 00:46:28 +0200

    Now returns x, how many kinds can be distributed to sister (not k, how many kinds there are). Added input validation that number of candies must be even.

--- 575_Distribute_Candies/DistributeCandies.java
+++ 575_Distribute_Candies/DistributeCandies.java
@@ -3,19 +3,30 @@ import java.util.Arrays;
 
 public final class DistributeCandies {
     public static void main(String... args) {
-        int[] testInput = {1,1,2,2,3,3};
-        distribute(testInput);
+        int[] testInput1 = {1,1,2,2,3,3};
+        distribute(testInput1);
+        int[] testInput2 = {1,1,2,3};
+        distribute(testInput2);
     }
 
-    public static long distribute(int[] input) {
-        int n = input.length;
+    public static Long distribute(int[] candies) {
+        Long x;
+        int n = candies.length;
+        // validate input
+        if (n % 2 != 0) {
+            throw new IllegalArgumentException("Number of candies must be even.");
+        }
 
-        Long k = IntStream.of(input)
+        // use a stream to count how many kinds of candy
+        Long k = IntStream.of(candies)
                 .distinct()
                 .count();
 
-        System.out.println(k);// expect 3
+        // this is the solution
+        x = (k >= n/2) ? n/2 : k;
 
-        return k;
+        System.out.println(x);// expect 3, then 2
+
+        return x;
     }
 }
